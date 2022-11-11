701d749c7a8bebd25d1e382913af1c567a53add8
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2022-11-12 00:14:53 +0100

    Implemented using a for loop

--- Lab-07-11-22/seleziona.sh
+++ Lab-07-11-22/seleziona.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-#grep A | wc -c
+#This method meanwhihle is very concise it makes you do this: cat filename | seleziona.sh
+#grep a | wc -c
 
 COUNT=0
 while read LINE; do
-    #Checking grep exit status FIX
-    grep a
-    if (($? == 0)); then
-        ((COUNT=$COUNT+${#LINE}))
+    #Checking if the current line has the char 'a' in any part of it
+    if [[ $LINE == *a* ]]; then
+        (( COUNT=$COUNT+${#LINE}+1 ))   #If we want to count \n like wc we should add + 1 here
     fi
 done < $1
+echo "$COUNT"
