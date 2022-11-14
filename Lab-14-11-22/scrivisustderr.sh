84f757e91e6c336597d90711a297ddbe487e8947
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2022-11-14 16:28:39 +0100

    Fixed regex bug

--- Lab-14-11-22/scrivisustderr.sh
+++ Lab-14-11-22/scrivisustderr.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 while read LINE; do
-    #Using "" to stop expansions such as *
-    #%% removes the longest suffix that matches the regex, * will be expanded in a space
-    echo "${LINE%%*}" 1>&2
+    # Using "" to stop expansions such as *
+    # %% removes the longest suffix that matches the regex,
+    # we want to get the first word of each line, therefore
+    # we look for a whitespace, followed by any character (*)
+    echo "${LINE%% *}" 1>&2
     echo "evviva"
 done
 
