24e817e2dea25910e5a293ab4d334034170a98d8
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2022-12-20 00:12:41 +0100

    Done.

--- Lab-22-12-19/sim-2017-giugno/esercizio2/contamulte.sh
+++ Lab-22-12-19/sim-2017-giugno/esercizio2/contamulte.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
+# Hint cat <file> | grep -oc <match>
+# -o prints the only-matching part of the string
+# -c counts the matched elements.
 
+PREVIOUS_VALUE=0
+
+while read NAME SURNAME VALUE OTHER; do
+	if [[ ${VALUE} == ${PREVIOUS_VALUE} ]]; then
+		continue
+	fi
+	PREVIOUS_VALUE=${VALUE}
+
+	echo -n "${VALUE} "
+	cat ./multe.txt | grep -oc ${VALUE}
+done < ./multe.txt
