5581d69b1863598521d10f85af3de7bd5288dd55
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2022-12-21 16:54:55 +0100

    Done.

---
+++ Home-Exercises/Shell/esercizio154-2020-07-17-2020-09-10-054/voti.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+### Nei commenti che seguono vedete dei suggerimenti sull'algoritmo da
+### seguire per produrre l'output corretto.
+###
+
+# I voti della seconda prova li devo mettere tutti nello standard output
+cat esame2.txt
+
+# Poi devo mettere nello standard output i voti della prima prova
+# ottenuti dagli studenti che non hanno un voto nella seconda prova
+
+while read ID GRADE; do
+	ret=`grep ${ID} esame2.txt`
+	if [[ ${ret} == "" ]]; then
+		echo "${ID} ${GRADE}"
+	fi
+done < ./esame1.txt
